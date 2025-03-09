b0023196419d23f5fa90c48c6ec354f4cd6bb938
Victor Gonçalves <victor@goncalvesmarques.com>
2025-03-09 09:45:43 -0300

    Create seed.ts

---
+++ src/tests/seed.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+async function main() {
+  const doctor = await prisma.doctor.create({
+    data: {
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+    },
+  });
+  // eslint-disable-next-line no-console
+  console.log('Doctor created:', doctor);
+}
+
+main()
+  .catch((e) => {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
