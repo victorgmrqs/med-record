721499768d9fb60e4a956443930259cb9924487b
Victor Gonçalves <@telefonicabigdata.com>
2025-03-10 22:30:13 -0300

    fix: resolving prisma migration

--- prisma/migrations/20250307224457_second_schema/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - The values [Fí] on the enum `Patient_sex` will be removed. If these variants are still used in the database, this will fail.
-
-*/
--- AlterTable
-ALTER TABLE `Patient` MODIFY `sex` ENUM('M', 'F') NOT NULL;
